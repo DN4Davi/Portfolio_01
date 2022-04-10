eb0cf19a4c68b20c92a56c05e1376e118cd9163b
Davi NA <davizinhonascimento@gmail.com>
2022-04-10 12:49:35 -0300

    Atualização de Estilo

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ let links = window.document.querySelectorAll(".internal-link");
 (Array.from(links)).map(link=>link.addEventListener('click', scroll))
 function scroll (e){
     e.preventDefault();
+    const header = window.document.querySelector('#header')
+    const headerHeight = header.getBoundingClientRect().height;
     const id = this.getAttribute("href");
     const section = window.document.querySelector(id);
     const position = section.getBoundingClientRect().top;
-    window.scrollBy({top: position - 80, left: 0, behavior: "smooth"});
+    window.scrollBy({top: position - headerHeight, left: 0, behavior: "smooth"});
 }
