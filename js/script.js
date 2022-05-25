4c3ed020d6b090718ba8deb780eb25ccf7bd0f7b
sigma-dna <104036233+sigma-dna@users.noreply.github.com>
2022-05-25 10:59:23 -0300

    mobile menu added

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ function scroll (e){
     const position = section.getBoundingClientRect().top;
     window.scrollBy({top: position - headerHeight, left: 0, behavior: "smooth"});
 }
+let menu = document.querySelector('#mobile.menu')
+menu.addEventListener('touchstart', menuToggle);
+function menuToggle() {
+    const thisParent = this.parentElement
+    thisParent.classList.toggle('active');
+    if(thisParent.classList.contains('active')){
+        document.documentElement.addEventListener('touchstart', outside);
+    }else{
+        document.documentElement.removeEventListener('touchstart', outside)
+    }
+}
+function outside (e) {if(!menu.parentElement.contains(e.target)){
+    menu.parentElement.classList.remove('active');
+    document.documentElement.removeEventListener('touchstart', outside)
+}}
